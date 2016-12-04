fbcf0af72c535fe42c583480f1cf398e3bea87c3
gburtonhome <banzetah@gmail.com>
2016-12-04 12:56:13 -0800

    added http POST, removed interval code, organized sections

--- BotanicalProject/main.js
+++ BotanicalProject/main.js
@@ -4,65 +4,86 @@
 /* jshint unused:true */
 
 "use strict" ;
-require('./LightSensor.js');
-//require('./motor.js');
-var mraa = require("mraa") ;
 
 // add any UPM requires that you need
-// and the rest of your app goes here
-// see the samples for more detailed examples
-
-console.log(mraa) ;     // prints mraa object to XDK IoT debug output panel
-
+require('./LightSensor.js');
+var mraa = require("mraa") ;
+// Load Grove module
+var groveSensor = require('jsupm_grove');
+//Create Buzzer instance
 var upmBuzzer = require("jsupm_buzzer");// Initialize on GPIO 5
 var myBuzzer = new upmBuzzer.Buzzer(5);
+// Create the temperature sensor object using AIO pin 0
+var temp = new groveSensor.GroveTemp(0);
+
+
+
+// and the rest of your app goes here
+//console.log(mraa) ;     // prints mraa object to XDK IoT debug output panel?????
+
+//Create Date instance
 var date = new Date(); 
 
-console.log("Date and Time:");
+//Date and Time
 var year = date.getFullYear();
 var month = date.getMonth()+1;
 var day = date.getDate();
 var hour = date.getHours();
-console.log(" " + year + " " + month + " " + day + " " + hour);
+var min = date.getMinutes();
+var sec = date.getSeconds();
+var fullDate = year + "." + month + "." + day + "." + hour + "." + min + "." + sec;
 
-//Sections of the day
+//Get instructions from DB on what to do at each Sections of the day
 var morning = 1;
 var afternoon = 2;
 var night = 3;
 var waterNow = 0; //set to 1 to turn on;
+//Tempurature readings in celcius and fahrenheit
+var celsius = temp.value();
+var fahrenheit = celsius * 9.0/5.0 + 32.0;
+console.log(celsius + " degrees Celsius, or " + Math.round(fahrenheit) + " degrees Fahrenheit");
 
-// Load Grove module
-var groveSensor = require('jsupm_grove');
-
-// Create the temperature sensor object using AIO pin 0
-var temp = new groveSensor.GroveTemp(0);
-console.log(temp.name());
-
-// Read the temperature ten times, printing both the Celsius and
-// equivalent Fahrenheit temperature, waiting one second between readings
-var i = 0;
-var waiting = setInterval(function() {
-            var celsius = temp.value();
-            var fahrenheit = celsius * 9.0/5.0 + 32.0;
-            console.log(celsius + " degrees Celsius, or " +
-                Math.round(fahrenheit) + " degrees Fahrenheit");
-            if (temp.value() > 22)
-            {
-                //Play sound for one second = 100000
-                console.log( myBuzzer.playSound(123, 50000) );
-            }
-            //Get full date and time in the form of datetime and convert to military time
-            i++;
-            if (i == 5) clearInterval(waiting);
-        }, 1000);
-
-
-// Print message when exiting
-process.on('SIGINT', function()
+//If temp reaches certain value DO SOMETHING
+if (temp.value() > 22)
 {
-	console.log("Exiting...");
-	process.exit(0);
-});
+    console.log( myBuzzer.playSound(123, 50000) );
+}
 
-var someVal = ls();
-console.log("someVal>>>>> " + someVal);
+//Get light value from LightSensor.js
+var luxValue = ls();
+console.log("luxValue>>>>> " + luxValue);
+
+//Get full date and time in the form of datetime and convert to military time - not necessary
+console.log("DATETIME (yyyy.mm.dd.hh.mm.ss: " + fullDate);
+
+var newDate = new Date();        
+var jsonObj = JSON.stringify({light:luxValue, temp:celsius, date:newDate, name:"Fred", username:"freddy123"});
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var http = new XMLHttpRequest();
+var url = "http://botanicaliot.herokuapp.com/plant-days";
+//var params = JSON.stringify(JSONData);
+http.open("POST", url, true);
+//Send the proper header information along with the request
+http.setRequestHeader("Content-Type", "application/json");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        console.log(http.responseText);
+        console.log("POST Success!!");
+    }
+    else
+    {
+        console.log("POST Failed. Http status: " + http.status)
+    }
+}
+http.send(jsonObj);
+
+myBuzzer.stopSound();
+console.log("Buzzer Stopped!!");
+
+
+
+
+
+
+
