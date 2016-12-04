7f4445afe8598a87a0ec085c7f3aa1fb6327381a
Taylor Pokoj <pokojt@gmail.com>
2016-12-03 18:48:45 -0800

    fix syntax errors

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-var token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoid2hhdGV2ZXIiLCJpZCI6IjU4NDM3ZmI3MjgzYmViNWM2ZmQzYjUyYyIsInJvbGVzIjpbXSwiaWF0IjoxNDgwODE4NjE1fQ.VvF7hVcjbytG7fGlefRM8LeOeEye_ZbsjcAxBEy36Ds;
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoid2hhdGV2ZXIiLCJpZCI6IjU4NDM3ZmI3MjgzYmViNWM2ZmQzYjUyYyIsInJvbGVzIjpbXSwiaWF0IjoxNDgwODE4NjE1fQ.VvF7hVcjbytG7fGlefRM8LeOeEye_ZbsjcAxBEy36Ds';
 
 $(document).ready(function() {
 	$('form.form-add-plant').submit(function() {
@@ -12,7 +12,7 @@ $(document).ready(function() {
 			type: 'POST',
 			contentType: 'application/json',
 			url: 'localhost:8090/plants',
-			data: JSON.stringify({name:'Fred', waterNeeds: 'high'});
+			data: JSON.stringify({name:'Fred', waterNeeds: 'high'})
 		}).done(function(data) {
 			console.log(data);
 		}).fail(function(jqxhr, status) {
