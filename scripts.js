c2dfa7d100342b274bc71cca37a80f9d3c38c541
Taylor Pokoj <pokojt@gmail.com>
2016-12-04 10:33:19 -0800

    Add code for pulling all plants from database and displaying on front end

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 
 var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoid2hhdGV2ZXIiLCJpZCI6IjU4NDM3ZmI3MjgzYmViNWM2ZmQzYjUyYyIsInJvbGVzIjpbXSwiaWF0IjoxNDgwODE4NjE1fQ.VvF7hVcjbytG7fGlefRM8LeOeEye_ZbsjcAxBEy36Ds';
 
+
+
 $(document).ready(function() {
 	$('.form-add-plant').hide();
 	$('button#add-plant').click(function() {
@@ -8,30 +10,44 @@ $(document).ready(function() {
 		$(this).hide();
 	});
 
-	// function for retrieving data from database and displaying on page
-	// $.get({
-	// 		headers: {
-	// 			Authorization: 'Bearer ' +  token
-	// 		},
-	// 		type: 'GET',
-	// 		contentType: 'application/json',
-	// 		url: '/plants',
-	// 		data: JSON.stringify({name:'Fred', waterNeeds: 'high'})
-	// 	}).done(function(data) {
-	// 		console.log(data);
-	// 	}).fail(function(jqxhr, status) {
-	// 		console.log(status);
-	// 	});
+	// function to display all plants in database on page
 
-	// $('#plantList').append()
+	$.ajax({
+				headers: {
+					Authorization: 'Bearer ' +  token
+				},
+				type: 'GET',
+				contentType: 'application/json',
+				url: '/plants',
+				success: function(data) {
+					var response = data;
+					console.log(response);
+					var plants = response.plants;
+					console.log(plants);
+					for(var i=0; i < plants.length; i++) {
+						$("#plantList").append("<div class='plant-item'>" +
+	                                  '<h3>' + plants[i].name + '</h3>' +
+	                              "</div>");
+	     				console.log(plants[i].name);
+					};
+				}
+			}).fail(function(jqxhr, status) {
+				console.log(jqxhr);
+				console.log(status);
+			});
+	
 
+
+	// funtion to add a new plant and save to database
 	$('form.form-add-plant').submit(function() {
 		event.preventDefault();
 
-		// var inputName = $('.nameInput').val();
-		// var inputType = $('.typeInput').val();
-		// var inputLight = $('.lightInput').val();
-		// var inputWater = $('.waterInput').val();
+
+
+		var inputName = $('.nameInput').val();
+		var inputType = $('.typeInput').val();
+		var inputLight = $('.lightInput').val();
+		var inputWater = $('.waterInput').val();
 		console.log('running');
 
 		$.ajax({
@@ -41,9 +57,12 @@ $(document).ready(function() {
 			type: 'POST',
 			contentType: 'application/json',
 			url: '/plants',
-			data: JSON.stringify({name:'Fred', waterNeeds: 'high'})
+			data: JSON.stringify({name:inputName, type: inputType, waterNeeds: inputWater,lightNeeds: inputLight})
 		}).done(function(data) {
 			console.log(data);
+			$("#plantList").append("<div class='plant-item'>" +
+	                                  '<h3>' + inputName + '</h3>' +
+	                              "</div>");
 		}).fail(function(jqxhr, status) {
 			console.log(status);
 		});
@@ -53,23 +72,5 @@ $(document).ready(function() {
  		$(".form-add-plant").hide();
  		$('button#add-plant').show();
 
-
-// $('form').on('submit', function(event) {
-// 	event.preventDefault();
-// 	var data = JSON.stringify({name: 'Fred', waterNeeds: 'high'});
-// 	$.ajax({
-// 		url: 'http://localhost:8090/plants',
-// 		type: 'GET',
-// 		// contentType: 'application/json',
-// 		headers: {
-// 			Authorization: 'Bearer ' +  token
-// 		},
-// 		// data: data,
-// 		success: function(data) {
-// 			console.log(data);
-// 		}
-// 	}).fail(function(jqxhr, status) {
-// 		console.log(jqxhr);
-// 		console.log(status);
-// 	});
+	});
 });
