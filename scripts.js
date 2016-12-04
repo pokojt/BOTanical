8f8a681daf6b099633c5a1e98ff8c6c48e9de4db
Taylor Pokoj <pokojt@gmail.com>
2016-12-04 09:33:41 -0800

    taylor's changes saturday

--- scripts.js
+++ scripts.js
@@ -2,8 +2,37 @@
 var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoid2hhdGV2ZXIiLCJpZCI6IjU4NDM3ZmI3MjgzYmViNWM2ZmQzYjUyYyIsInJvbGVzIjpbXSwiaWF0IjoxNDgwODE4NjE1fQ.VvF7hVcjbytG7fGlefRM8LeOeEye_ZbsjcAxBEy36Ds';
 
 $(document).ready(function() {
-	$('form.form-add-plant').submit(function() {
+	$('.form-add-plant').hide();
+	$('button#add-plant').click(function() {
+		$('.form-add-plant').show();
+		$(this).hide();
+	});
 
+	// function for retrieving data from database and displaying on page
+	// $.get({
+	// 		headers: {
+	// 			Authorization: 'Bearer ' +  token
+	// 		},
+	// 		type: 'GET',
+	// 		contentType: 'application/json',
+	// 		url: '/plants',
+	// 		data: JSON.stringify({name:'Fred', waterNeeds: 'high'})
+	// 	}).done(function(data) {
+	// 		console.log(data);
+	// 	}).fail(function(jqxhr, status) {
+	// 		console.log(status);
+	// 	});
+
+	// $('#plantList').append()
+
+	$('form.form-add-plant').submit(function() {
+		event.preventDefault();
+
+		// var inputName = $('.nameInput').val();
+		// var inputType = $('.typeInput').val();
+		// var inputLight = $('.lightInput').val();
+		// var inputWater = $('.waterInput').val();
+		console.log('running');
 
 		$.ajax({
 			headers: {
@@ -11,13 +40,18 @@ $(document).ready(function() {
 			},
 			type: 'POST',
 			contentType: 'application/json',
-			url: 'localhost:8090/plants',
+			url: '/plants',
 			data: JSON.stringify({name:'Fred', waterNeeds: 'high'})
 		}).done(function(data) {
 			console.log(data);
 		}).fail(function(jqxhr, status) {
 			console.log(status);
 		});
+
+
+		$(".form-add-plant")[0].reset();
+ 		$(".form-add-plant").hide();
+ 		$('button#add-plant').show();
 	});
 
 });
